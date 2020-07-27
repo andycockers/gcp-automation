6a14e89f3edb1e2e011f6aba39445674029bdc54
Andy Cockerill <34304883+andycockers@users.noreply.github.com>
2020-07-27 14:25:16 +0100

    updated sh file

--- ansible/roles/config-squid3-client/files/proxy.sh
+++ ansible/roles/config-squid3-client/files/proxy.sh
@@ -1,4 +1,4 @@
-export http_proxy=\"http://gateway-instance.$(dnsdomainname):3128
-export https_proxy=\"http://gateway-instance.$(dnsdomainname):3128
-export ftp_proxy=\"http://gateway-instance.$(dnsdomainname):3128
+export http_proxy=\"http://squid.$(dnsdomainname):3128
+export https_proxy=\"http://squid.$(dnsdomainname):3128
+export ftp_proxy=\"http://squid.$(dnsdomainname):3128
 export no_proxy=169.254.169.254,metadata,metadata.google.internal
