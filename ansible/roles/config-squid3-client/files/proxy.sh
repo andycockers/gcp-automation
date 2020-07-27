59cabf4f8500ef956850f7c08766bf0942938960
Andy Cockerill <34304883+andycockers@users.noreply.github.com>
2020-07-27 16:11:26 +0100

    changed config

--- ansible/roles/config-squid3-client/files/proxy.sh
+++ ansible/roles/config-squid3-client/files/proxy.sh
@@ -1,4 +1,4 @@
-export http_proxy=\"http://squid.$(dnsdomainname):3128
-export https_proxy=\"http://squid.$(dnsdomainname):3128
-export ftp_proxy=\"http://squid.$(dnsdomainname):3128
+export http_proxy="http://squid.us-central1-a.c.jenkins-test-project-284609.internal:3128"
+export https_proxy="http://squid.us-central1-a.c.jenkins-test-project-284609.internal:3128"
+export ftp_proxy="http://squid.us-central1-a.c.jenkins-test-project-284609.internal:3128"
 export no_proxy=169.254.169.254,metadata,metadata.google.internal
