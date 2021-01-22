e6b105fdb5f1d13cb454442b6bd4836b1a45439d
Andy Cockerill <andrew.cockerill@appnovation.com>
2021-01-22 20:15:07 +0000

    changes

--- shell/apk/install-sshd/install.sh
+++ shell/apk/install-sshd/install.sh
@@ -2,10 +2,12 @@ apk update
 
 apk add openssh openrc
 
+rc-update add sshd
+
+rc-status
+
 /etc/init.d/sshd start || true
 
 touch /run/openrc/softlevel
 
 /etc/init.d/sshd start || true
-
-rc-update add sshd
