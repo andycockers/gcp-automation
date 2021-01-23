e8b00678df0e41f8675d611d22a7b5ce9e660f0e
Andy Cockerill <andrew.cockerill@appnovation.com>
2021-01-23 11:35:35 +0000

    ouch

--- shell/ansible/install-docker-worker-deps-arm.sh
+++ shell/ansible/install-docker-worker-deps-arm.sh
@@ -1,3 +1,3 @@
 cd /tmp/ansible
 
-ansible-playbook -c local -i localhost, install-docker-worker-deps-arm.yml
+ansible-playbook -c local -i localhost, install-docker-worker-deps-arm.yml -e 'ansible_python_interpreter=/usr/bin/python3'
