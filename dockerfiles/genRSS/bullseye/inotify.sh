ebb4ea7fab012480848ce58a7d8aebce51bedb60
cockers <cockerill.andrew@gmail.com>
2022-07-06 14:34:52 +0100

    genrss

--- dockerfiles/genRSS/bullseye/inotify.sh
+++ dockerfiles/genRSS/bullseye/inotify.sh
@@ -3,6 +3,6 @@
 while : 
 do
   inotifywatch -v -e moved_to -e close_write -r /var/www && {
-    python3 genRSS.py -e "mp3,ogg" -d /var/www -H https://pod.crabdance.com -t "Cockers" -p "Radio" -r -o feed.rss
+    python3 /usr/bin/genRSS.py -e "mp3,ogg" -d /var/www -H https://pod.crabdance.com -t "Cockers" -p "Radio" -r -o feed.rss
   }
 done
