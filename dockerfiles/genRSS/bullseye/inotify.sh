a0eb3d7cfce8bcc578df9353bcad93a1c808f7fa
cockers <cockerill.andrew@gmail.com>
2022-07-18 14:19:47 +0100

    removed mp2

--- dockerfiles/genRSS/bullseye/inotify.sh
+++ dockerfiles/genRSS/bullseye/inotify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 /usr/bin/genRSS.py -e "mp3,ogg,aac,mp2" -d radio -H https://pod.crabdance.com -t "Cockers" -p "Radio" -r -o feed.rss
+python3 /usr/bin/genRSS.py -e "mp3,ogg,aac" -d radio -H https://pod.crabdance.com -t "Cockers" -p "Radio" -r -o feed.rss
 
 python3 /usr/bin/cleanup.py
