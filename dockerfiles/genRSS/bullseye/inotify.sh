d8ccae53bb8918c86f5e8a68bc662232d082eb5d
cockers <cockerill.andrew@gmail.com>
2022-08-01 15:42:48 +0100

    added ts

--- dockerfiles/genRSS/bullseye/inotify.sh
+++ dockerfiles/genRSS/bullseye/inotify.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-python3 /usr/bin/genRSS.py -e "mp3,ogg,aac" -d did -H https://pod.crabdance.com -t "Desert-Island-Discs" -p "Radio" -r -o did.rss
+python3 /usr/bin/genRSS.py -e "mp3,ogg,aac,ts" -d did -H https://pod.crabdance.com -t "Desert-Island-Discs" -p "Radio" -r -o did.rss
 
-python3 /usr/bin/genRSS.py -e "mp3,ogg,aac" -d fnc -H https://pod.crabdance.com -t "Friday-Night-Comedy" -p "Radio" -r -o fnc.rss
+python3 /usr/bin/genRSS.py -e "mp3,ogg,aac,ts" -d fnc -H https://pod.crabdance.com -t "Friday-Night-Comedy" -p "Radio" -r -o fnc.rss
 
-python3 /usr/bin/genRSS.py -e "mp3,ogg,aac" -d ftw -H https://pod.crabdance.com -t "The-First-Time-With" -p "Radio" -r -o ftw.rss
+python3 /usr/bin/genRSS.py -e "mp3,ogg,aac,ts" -d ftw -H https://pod.crabdance.com -t "The-First-Time-With" -p "Radio" -r -o ftw.rss
 
-python3 /usr/bin/genRSS.py -e "mp3,ogg,aac" -d kermode -H https://pod.crabdance.com -t "Kermode" -p "Radio" -r -o kermode.rss
+python3 /usr/bin/genRSS.py -e "mp3,ogg,aac,ts" -d kermode -H https://pod.crabdance.com -t "Kermode" -p "Radio" -r -o kermode.rss
 
-python3 /usr/bin/genRSS.py -e "mp3,ogg,aac" -d roundtable -H https://pod.crabdance.com -t "Roundtable" -p "Radio" -r -o rt.rss
+python3 /usr/bin/genRSS.py -e "mp3,ogg,aac,ts" -d roundtable -H https://pod.crabdance.com -t "Roundtable" -p "Radio" -r -o rt.rss
 
 python3 /usr/bin/cleanup.py did
 python3 /usr/bin/cleanup.py fnc
